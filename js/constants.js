4ad535e06374364012ef493c56af1d3c499efeb1
RinatShakirov <62038576+RinatShakirov@users.noreply.github.com>
2021-06-11 12:05:39 +0200

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+const GEODES = document.querySelector('.geodes');
+const GEOLOG = document.querySelector('.geolog');
+const ECHOLOG = document.querySelector('.echolog');
+const GEOPHYSICK = document.querySelector('.geophysick');
+const HYDROMETEO = document.querySelector('.hydrometeo');
+
+const INFOINNER = document.querySelector('.info_inner');
+
